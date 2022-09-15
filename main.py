58cdda1d2dce51a86769f6f0edff509efe0a22db
Samb <samb@townsware.com>
2022-09-15 11:56:47 -0400

    Sections 1,2,3 - collected information of 5 stocks in the past 10 days

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 """
+(5) Initial comments with your name, class and project at the top of your .py file:
+
 INF601 - Advanced Programming in Python
 Sam Boutros
 Mini Project 1
 9/15/2022
 """
 
-# Importing the used packages
+
+# (5) Proper import of packages used:
 import numpy as np
 import matplotlib as plt
 import yfinance as yf
+import datetime as dt
+
+
+# (20) Collect the closing price of 5 of your favorite stock tickers for the last 10 trading days
+myStockList = ["ITOT","BA","UNH","BAC","COIN"]
+countOfDays = 10
+enddate = dt.date.today()
+startdate = enddate + dt.timedelta(days=-30)
+
+print() # Blank line
+print("Collecting the closing price of the following",len(myStockList),"stock tickers for the last",countOfDays,"trading days:")
+for stock in myStockList:
+    print("    ",stock)
+rawData = [] # Create empty list (native 1d Py array), each list element will be raw data of one stock
+for stock in myStockList:
+    datum = yf.download(stock, start=startdate, end=enddate)
+    rawData.append(datum[-countOfDays:]) # Add only the last "countOfDays" to our rawData list
+print("done")
+
+
+# (10) Store this information in a list that you will convert to a ndarray in NumPy
+print()
+
 
 # Get MSFt data
-data = yf.download("MSFT", start="2022-09-01", end="2022-09-30")
+data = yf.download("MSFT", start="2022-08-01", end="2022-09-30")
 
 print("Got",len(data),"days worth of MSFT stock price data.")
-
+print(data[-10:])
 # Create empty list (native 1d Py array):
 msftPrices = []
 
