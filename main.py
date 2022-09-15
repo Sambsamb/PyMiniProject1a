84c348c4b72e30539a21cddd13d131f4ca715b48
Samb <samb@townsware.com>
2022-09-15 15:24:06 -0400

    Rewrite to use lists instead of dataframes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 """
 (5) Initial comments with your name, class and project at the top of your .py file:
-
 INF601 - Advanced Programming in Python
 Sam Boutros
 Mini Project 1
@@ -17,40 +16,35 @@ import os
 
 
 # (20) Collect the closing price of 5 of your favorite stock tickers for the last 10 trading days
-myStockList = ["ITOT","BA","UNH","BAC","COIN"]
+myStockList = ["ITOT", "BA", "UNH", "BAC", "COIN"]
 countOfDays = 10
 enddate = dt.date.today()
 startdate = enddate + dt.timedelta(days=-30)
-
-print() # Blank line
-print("Collecting the closing price of the following",len(myStockList),"stock tickers for the last",countOfDays,"trading days:")
+print()  # Blank line
+print("Collecting the closing price of the following", len(myStockList), "stock tickers for the last", countOfDays, "trading days:")
 myVars = locals()
 for stock in myStockList:
     datum = yf.download(stock, start=startdate, end=enddate)
     lastDays = datum[-countOfDays:]
-    stockValue = lastDays['Adj Close']
-    myVars[stock] = stockValue
+    myVars[stock] = []  # Declare a 'local' variable with the name of the current stock as a Py list
+    for price in lastDays['Adj Close']:
+        myVars[stock].append(price)
     print()
-    print("    Stock",stock)
+    print("    Stock", stock, "last", countOfDays, "days price")
     print(myVars[stock])
-print("Data is stored in",type(myVars[stock])) # Data is stored in <class 'pandas.core.series.Series'>
-
-"""
-# (10) Store this information in a list that you will convert to a ndarray in NumPy
-myNumPyArray = np.array([rawData[0], rawData[1], rawData[2], rawData[3], rawData[4]])
-# Technically the input rawData[0] is <class 'pandas.core.frame.DataFrame'> and not <class 'list'> but the resulting NumPy ndarray works the same
-
 print()
-print("Loaded up the raw data into a NumPy array",'"myNumPyArray"',type(myNumPyArray))
-print("This is a 3 dimentional array, with the following shape/dimensions:",myNumPyArray.shape)
-print("The first dimension is the 'stock', for example the metrics of all ten days of the fifth stock 'COIN' can be referenced as 'myNumPyArray[4]'")
-print(myNumPyArray[4]) # Data of the last stock 'COIN' (all 10 days, all 6 metrics)
-print("The second dimension is the 'date', for example metrics of the tenth day the fifth stock 'COIN' can be referenced as 'myNumPyArray[4,9]':")
-print(myNumPyArray[4,9]) # Metrics (Open, High, Low, Close, Adj Close, Volume) of the tenth day of the fifth stock 'COIN'
-print("The third dimension is a specific metric of a specific date of a specific stock, for example the 'Adj Close' of the tenth day the fifth stock 'COIN' can be referenced as 'myNumPyArray[4,9,4]':")
-print(myNumPyArray[4,9,4]) # 'Adj Close' of the tenth day of 'COIN'
-print("The following expression returns the 'Adj Close' of all 10 days of the fifth stock 'COIN' 'myNumPyArray[4,:10,4]':")
-print(myNumPyArray[4,:10,4]) # 'Adj Close' of all 10 days of 'COIN'
+print("Data is stored as", type(myVars[stock]))  # Data is stored in <class 'list'>
+
+
+# (10) Store this information in a list that you will convert to a ndarray in NumPy
+myNumPyArray = np.array([ITOT, BA, UNH, BAC, COIN])
+print()
+print("Loaded up the raw data into a NumPy array", '"myNumPyArray"', type(myNumPyArray))
+print("This is a 2 dimensional array, with the following shape/dimensions:", myNumPyArray.shape)
+print("The first dimension is the Price, for example the price of all ten days of the fifth stock 'COIN' can be referenced as 'myNumPyArray[4]'")
+print(myNumPyArray[4])  # Prices of the last stock 'COIN' (all 10 days)
+print("The second dimension is the Price of a specific Date, for example the Price of the tenth day of the fifth stock 'COIN' can be referenced as 'myNumPyArray[4,9]'")
+print(myNumPyArray[4, 9])  # Price of the tenth day of the fifth stock 'COIN'
 
 
 # (10) Plot these 5 graphs
@@ -60,10 +54,9 @@ if not os.path.exists('charts'):
 print()
 i = 0
 for stock in myStockList:
-    print("Plotting 'Adj Close' values of the last",countOfDays,"days of stock",stock)
-    plt.plot(myNumPyArray[i,:10,4])
+    print("Plotting 'Adj Close' values of the last", countOfDays, "days of stock", stock)
+    plt.plot(myNumPyArray[i])
     plt.savefig('charts/' + stock + '.png')
     plt.show()
     i += 1
 
-"""
